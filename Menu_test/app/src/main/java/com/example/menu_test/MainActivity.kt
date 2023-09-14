4fb54545938fc950829f3c4d6837df78084de8c6
wnsdnn <a24369029@gmail.com>
2023-09-14 23:58:23 +0900

    option menu xml파일로 만들어보기

--- Menu_test/app/src/main/java/com/example/menu_test/MainActivity.kt
+++ Menu_test/app/src/main/java/com/example/menu_test/MainActivity.kt
@@ -16,8 +16,10 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val menuItem1: MenuItem? = menu?.add(0, 0, 0, "menu1")
-        val menuItem2: MenuItem? = menu?.add(0, 1, 0, "menu2")
+//        val menuItem1: MenuItem? = menu?.add(0, 0, 0, "menu1")
+//        val menuItem2: MenuItem? = menu?.add(0, 1, 0, "menu2")
+
+        menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
