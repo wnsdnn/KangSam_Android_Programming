f04e8dbf1d4cdd8e832ed760b9fa63ebfbf7f193
wnsdnn <a24369029@gmail.com>
2023-09-16 00:00:34 +0900

    액션바 searchView 사용해보기

--- Menu_test/app/src/main/java/com/example/menu_test/MainActivity.kt
+++ Menu_test/app/src/main/java/com/example/menu_test/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +21,29 @@ class MainActivity : AppCompatActivity() {
 //        val menuItem1: MenuItem? = menu?.add(0, 0, 0, "menu1")
 //        val menuItem2: MenuItem? = menu?.add(0, 1, 0, "menu2")
 
-        menuInflater.inflate(R.menu.menu_main, menu)
+        // 일반 메뉴
+//        menuInflater.inflate(R.menu.menu_main, menu)
+
+        // 검색 메뉴
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        val menuItem = menu?.findItem(R.id.menu_search)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 키보드의 검색 버튼을 클릭한 순간의 이벤트
+                Toast.makeText(this@MainActivity, "$query", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // 검색어 변경 이벤트
+                Log.d("kkang", "$newText")
+                return true
+            }
+
+        })
+
         return super.onCreateOptionsMenu(menu)
     }
 
