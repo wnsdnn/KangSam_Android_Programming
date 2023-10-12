e026d1763377986df8fdc8d1d750d849bc9cc5c1
wnsdnn <a24369029@gmail.com>
2023-10-13 00:14:22 +0900

    기본적인 Tab 기능 오나성

--- TabLayout/app/src/main/java/com/example/tablayout/MainActivity.kt
+++ TabLayout/app/src/main/java/com/example/tablayout/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.tablayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.example.tablayout.Fragments.OneFragment
+import com.example.tablayout.Fragments.ThreeFragment
+import com.example.tablayout.Fragments.TwoFragment
 import com.example.tablayout.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -28,8 +31,38 @@ class MainActivity : AppCompatActivity() {
         tabLayout.addTab(tab2)
 
         val tab3: TabLayout.Tab = tabLayout.newTab()
-        tab3.text = "Tab1"
+        tab3.text = "Tab3"
         tabLayout.addTab(tab3)
+
+
+        // 기본값 처리
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.tabContent, OneFragment())
+        transaction.commit()
+
+        // 탭 버튼 이벤트 처리
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // 탭 버튼을 선택할 때 이벤트
+                val transaction = supportFragmentManager.beginTransaction()
+
+                when(tab?.text) {
+                    "Tab1" -> transaction.replace(R.id.tabContent, OneFragment())
+                    "Tab2" -> transaction.replace(R.id.tabContent, TwoFragment())
+                    "Tab3" -> transaction.replace(R.id.tabContent, ThreeFragment())
+                }
+                transaction.commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // 다른 탭 버튼을 눌러 선택된 탭 버튼이 해제될 때 이벤트
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // 선택된 탭 버튼을 다시 선택할 때 이벤트
+            }
+
+        })
 
     }
 }
