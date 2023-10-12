4984e3711daeebd31b1f6cba50b273dbb65f29de
wnsdnn <a24369029@gmail.com>
2023-10-13 00:06:56 +0900

    TabMenu 만듦

--- TabLayout/app/src/main/java/com/example/tablayout/MainActivity.kt
+++ TabLayout/app/src/main/java/com/example/tablayout/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.tablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.tablayout.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        val tabLayout = binding.tabs
+
+        val tab1: TabLayout.Tab = tabLayout.newTab()
+        tab1.text = "Tab1"
+        tabLayout.addTab(tab1)
+
+        val tab2: TabLayout.Tab = tabLayout.newTab()
+        tab2.text = "Tab2"
+        tabLayout.addTab(tab2)
+
+        val tab3: TabLayout.Tab = tabLayout.newTab()
+        tab3.text = "Tab1"
+        tabLayout.addTab(tab3)
+
     }
 }
