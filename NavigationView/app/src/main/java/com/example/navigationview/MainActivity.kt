e85ff9885a0f48ebe9f8248094104725ea277c90
wnsdnn <a24369029@gmail.com>
2023-10-17 01:04:06 +0900

    NavigationView를 이용해 메뉴 추가해보기

--- NavigationView/app/src/main/java/com/example/navigationview/MainActivity.kt
+++ NavigationView/app/src/main/java/com/example/navigationview/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.navigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.navigationview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.drawerView.setNavigationItemSelectedListener { it ->
+            Toast.makeText(this, "${it.title}", Toast.LENGTH_SHORT).show()
+
+            true
+        }
+
+
     }
 }
