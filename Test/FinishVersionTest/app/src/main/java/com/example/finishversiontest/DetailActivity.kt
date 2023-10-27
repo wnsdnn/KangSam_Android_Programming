1cf1a3f29d47ef22a5c4a24a9567be83c9543ea8
wnsdnn <a24369029@gmail.com>
2023-10-28 00:09:25 +0900

    뭔가 하는 중

--- Test/FinishVersionTest/app/src/main/java/com/example/finishversiontest/DetailActivity.kt
+++ Test/FinishVersionTest/app/src/main/java/com/example/finishversiontest/DetailActivity.kt
@@ -2,10 +2,21 @@ package com.example.finishversiontest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.finishversiontest.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
     }
 }
--- Test/FinishVersionTest/app/src/main/java/com/example/finishversiontest/MainActivity.kt
+++ Test/FinishVersionTest/app/src/main/java/com/example/finishversiontest/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.finishversiontest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.finishversiontest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.detailBtn.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        binding.finishBtn.setOnClickListener {
+            finish()
+        }
+
     }
 }
