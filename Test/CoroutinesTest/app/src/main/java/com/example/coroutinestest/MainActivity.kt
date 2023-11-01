bb168ab3ad9fbb7d1747f5bd798c2bf20a992115
wnsdnn <a24369029@gmail.com>
2023-11-02 00:52:58 +0900

    시간이 오래걸리는 작업 코드 커밋

--- Test/CoroutinesTest/app/src/main/java/com/example/coroutinestest/MainActivity.kt
+++ Test/CoroutinesTest/app/src/main/java/com/example/coroutinestest/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.coroutinestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.coroutinestest.databinding.ActivityMainBinding
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val startBtn = binding.startBtn
+        val resultText = binding.resultText
+        var sum = 0L
+
+        startBtn.setOnClickListener {
+            val time = measureTimeMillis {
+                for (i in 1..2_000_000_000) {
+                    sum += i
+                }
+            }
+
+            Log.d("kkang", "time: $time")
+            resultText.text = "sum: $sum"
+        }
+
     }
 }
