86c9d6c2d83d654e637c948bd0e323da42c4a994
wnsdnn <a24369029@gmail.com>
2023-11-09 23:34:51 +0900

    Ch14 따라만들기 완성

--- Chapter/Ch14_Receiver/app/src/main/java/com/example/ch14_receiver/MainActivity.kt
+++ Chapter/Ch14_Receiver/app/src/main/java/com/example/ch14_receiver/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.example.ch14_receiver
 
+import android.Manifest
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.BatteryManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.example.ch14_receiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            if (it.all {permission -> permission.value == true}) {
+                val intent = Intent(this, MyReceiver::class.java)
+                sendBroadcast(intent)
+            } else {
+                Toast.makeText(this@MainActivity, "permission denied...", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))!!.apply {
+            when (getIntExtra(BatteryManager.EXTRA_STATUS, -1)) {
+                // 배터리 충전 상태일때
+                BatteryManager.BATTERY_STATUS_CHARGING -> {
+                    when (getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)) {
+                        // 고속 충전일때
+                        BatteryManager.BATTERY_PLUGGED_USB -> {
+                            binding.chargingResultView.text = "USB Plugged"
+                            binding.chargingImageView.setImageBitmap(BitmapFactory.decodeResource(resources, R.drawable.usb))
+                        }
+                        // 저속 충전일때
+                        BatteryManager.BATTERY_PLUGGED_AC -> {
+                            binding.chargingResultView.text = "AC Plugged"
+                            binding.chargingImageView.setImageBitmap(BitmapFactory.decodeResource(resources, R.drawable.ac))
+                        }
+                    }
+                }
+                else -> {
+                    binding.chargingResultView.text = "Not Plugged"
+                }
+            }
+
+            val level = getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            val batteryPct = (level.toFloat() / scale.toFloat()) * 100
+            binding.percentResultView.text = "$batteryPct%"
+        }
+
+        binding.button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                val postNotifi = Manifest.permission.POST_NOTIFICATIONS
+                if (ContextCompat.checkSelfPermission(this, postNotifi) == PackageManager.PERMISSION_GRANTED) {
+                    val intent = Intent(this, MyReceiver::class.java)
+                    sendBroadcast(intent)
+                } else {
+                    permissionLauncher.launch(arrayOf(postNotifi))
+                }
+            } else {
+                val intent = Intent(this, MyReceiver::class.java)
+                sendBroadcast(intent)
+            }
+        }
+
+
     }
 }
